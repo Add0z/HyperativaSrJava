80853992b227fea3763c6015362efb871a87c48e
André da Silveira Pereira <33392383+Add0z@users.noreply.github.com>
2026-02-15 11:46:50 -0300

    refactor(audit): remove old audit files from legacy location

--- javaEspecialista/src/main/java/com/hyperativa/javaEspecialista/adapters/out/persistence/repo/AuditLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hyperativa.javaEspecialista.adapters.out.persistence.repo;
-
-import com.hyperativa.javaEspecialista.adapters.out.persistence.entity.AuditLogEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuditLogRepository extends CrudRepository<AuditLogEntity, Long> {
-}
